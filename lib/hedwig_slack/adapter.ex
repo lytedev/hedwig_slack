bd16697d8a67a844c44208048593ebc812e2ec8f
Daniel Flanagan <daniel@lytedev.io>
2019-09-19 17:20:10 -0500

    Take 2

--- lib/hedwig_slack/adapter.ex
+++ lib/hedwig_slack/adapter.ex
@@ -24,7 +24,7 @@ defmodule Hedwig.Adapters.Slack do
     {:ok, %State{opts: opts, robot: robot, token: token}}
   end
 
-  def handle_cast({:send, {msg, overrides}}, %{conn: conn} = state) do
+  def handle_cast({:send, %{opts: %{} = overrides} = msg}, %{conn: conn} = state) do
     Connection.ws_send(conn, slack_message(msg, overrides))
     {:noreply, state}
   end
